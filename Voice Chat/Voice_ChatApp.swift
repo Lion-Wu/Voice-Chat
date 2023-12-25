20cd4b6eb663878e7c76b3aeb7d3f44a50a44c36
Wu Zichen <wuwu0808080808@icloud.com>
2023-12-25 13:24:16 +0800

    Initial Commit

---
+++ Voice Chat/Voice_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Voice_ChatApp.swift
+//  Voice Chat
+//
+//  Created by 吴子宸 on 2023/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Voice_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
