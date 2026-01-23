fd19fab8cd603c4813648dda09d80c8adc2c3e16
Lion Wu <drain.daters.0p@icloud.com>
2026-01-23 08:57:47 +0800

    adjust message editing UI

--- Voice Chat/Views/ChatViewComponents/PreferenceKeys.swift
+++ Voice Chat/Views/ChatViewComponents/PreferenceKeys.swift
@@ -27,3 +27,10 @@ struct BottomAnchorKey: PreferenceKey {
         value = nextValue()
     }
 }
+
+struct EditingBannerHeightKey: PreferenceKey {
+    static var defaultValue: CGFloat { 0 }
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = max(value, nextValue())
+    }
+}
